29d5c74773c4552bade643e0e1f82c8b216f3201
freelon <freelon@gmx.net>
2021-12-03 10:07:19 +0100

    Day 3 part 1.

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,5 +1,48 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(args) do
+    lines =
+      args
+      |> String.trim()
+      |> String.split("\n")
+
+    line_count = length(lines)
+    [first | _] = lines
+    width = String.length(first)
+
+    zeros_at_indexes =
+      lines
+      |> Enum.reduce(%{}, fn line, acc ->
+        String.graphemes(line)
+        |> Enum.with_index(fn element, index -> {index, element} end)
+        |> Enum.reduce(acc, fn {index, char}, acc ->
+          if char == "0" do
+            Map.put(acc, index, Map.get(acc, index, 0) + 1)
+          else
+            acc
+          end
+        end)
+      end)
+
+    gamma =
+      0..(width - 1)
+      |> Enum.map(fn index ->
+        zeros = zeros_at_indexes[index]
+
+        if zeros > line_count / 2 do
+          "0"
+        else
+          "1"
+        end
+      end)
+
+    epsilon =
+      Enum.map(gamma, fn c ->
+        if c == "0", do: "1", else: "0"
+      end)
+
+    epsilon
+
+    String.to_integer(Enum.join(gamma), 2) * String.to_integer(Enum.join(epsilon), 2)
   end
 
   def part2(_args) do
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -3,17 +3,27 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  defp input, do: "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
 
-    assert result
+  test "part1" do
+    result = part1(input())
+
+    assert result == 198
   end
 
   @tag :skip
   test "part2" do
-    input = nil
     result = part2(input)
 
     assert result
