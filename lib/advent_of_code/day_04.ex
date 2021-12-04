217e99a3948299a844d2b8648507f3ff50488d49
freelon <freelon@gmx.net>
2021-12-04 19:21:43 +0100

    Improved speed of Day 4 part 2.

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -31,15 +31,29 @@ defmodule AdventOfCode.Day04 do
       |> Enum.filter(fn {{fx, _}, _} -> fx == x end)
       |> Enum.sort_by(fn {{_, y}, _} -> y end)
     end
+
+    def row_empty(board, y) do
+      not (board.fields |> Enum.any?(fn {{_, fy}, _} -> fy == y end))
+    end
+
+    def col_empty(board, x) do
+      not (board.fields |> Enum.any?(fn {{fx, _}, _} -> fx == x end))
+    end
   end
 
   def part1(args) do
     {numbers, boards} = parse(args)
 
-    {checked, [winner|_]} = get_first_bingo([], numbers, boards)
+    {checked, [winner | _]} = get_first_bingo([], numbers, boards)
 
     :logger.debug("checked: #{hd(checked)}")
-    sum = winner.fields |> Enum.map(fn {_, n} -> n end) |> Enum.filter(fn n -> not Enum.member?(checked, n) end) |> Enum.sum()
+
+    sum =
+      winner.fields
+      |> Enum.map(fn {_, n} -> n end)
+      |> Enum.filter(fn n -> not Enum.member?(checked, n) end)
+      |> Enum.sum()
+
     :logger.debug("unmarked sum: #{sum}")
     hd(checked) * sum
   end
@@ -47,7 +61,11 @@ defmodule AdventOfCode.Day04 do
   #  returns {list of checked, list of boards with bingo}
   defp get_first_bingo(checked, open, boards) do
     xyz = [hd(open)] ++ checked
-    :logger.debug("checked: #{inspect checked, charlists: :as_lists} - open: #{inspect open, charlists: :as_lists}")
+
+    :logger.debug(
+      "checked: #{inspect(checked, charlists: :as_lists)} - open: #{inspect(open, charlists: :as_lists)}"
+    )
+
     bingos = boards |> Enum.filter(&is_bingo(xyz, &1))
 
     if Enum.empty?(bingos) do
@@ -58,8 +76,11 @@ defmodule AdventOfCode.Day04 do
   end
 
   defp is_bingo(numbers, board) do
-    all_rows_cols = (0..board.width-1 |> Enum.map(&Board.get_row(board, &1))) ++ (0..board.width-1 |> Enum.map(&Board.get_col(board, &1)))
-    Enum.any?(all_rows_cols, &Enum.all?(&1, fn {_,n} -> Enum.member?(numbers, n) end))
+    all_rows_cols =
+      (0..(board.width - 1) |> Enum.map(&Board.get_row(board, &1))) ++
+        (0..(board.width - 1) |> Enum.map(&Board.get_col(board, &1)))
+
+    Enum.any?(all_rows_cols, &Enum.all?(&1, fn {_, n} -> Enum.member?(numbers, n) end))
   end
 
   defp parse(args) do
@@ -86,23 +107,43 @@ defmodule AdventOfCode.Day04 do
   def part2(args) do
     {numbers, boards} = parse(args)
 
-    {checked, [winner|_]} = get_last_bingo([], numbers, boards)
+    {checked, winner} = get_last_bingo(numbers, boards)
 
-    :logger.debug("checked: #{hd(checked)}")
-    sum = winner.fields |> Enum.map(fn {_, n} -> n end) |> Enum.filter(fn n -> not Enum.member?(checked, n) end) |> Enum.sum()
+    :logger.debug("checked: #{checked}")
+
+    sum =
+      winner.fields
+      |> Enum.map(fn {_, v} -> v end)
+      |> Enum.sum()
+
     :logger.debug("unmarked sum: #{sum}")
-    hd(checked) * sum
+    checked * sum
   end
 
-  defp get_last_bingo(checked, open, boards) do
-    xyz = [hd(open)] ++ checked
-    :logger.debug("checked: #{inspect checked, charlists: :as_lists} - open: #{inspect open, charlists: :as_lists}")
-    {bingo, non_bingo} = boards |> Enum.split_with(&is_bingo(xyz, &1))
+  # returns a tuple {winning_number, winning_board}
+  defp get_last_bingo(open, boards) do
+    [head | remaining] = open
+
+    {bingo, non_bingo} =
+      boards
+      |> Enum.map(fn b ->
+        cross_out(b, head)
+      end)
+      |> Enum.split_with(&is_bingo(&1))
 
     if Enum.empty?(non_bingo) do
-      {xyz, bingo}
+      {head, hd(bingo)}
     else
-      get_last_bingo(xyz, tl(open), non_bingo)
+      get_last_bingo(remaining, non_bingo)
     end
+  end
+
+  defp cross_out(board, value) do
+    %{board | fields: :maps.filter(fn _, v -> v != value end, board.fields)}
+  end
+
+  defp is_bingo(board) do
+    0..(board.width - 1)
+    |> Enum.any?(&(Board.row_empty(board, &1) or Board.col_empty(board, &1)))
   end
 end
