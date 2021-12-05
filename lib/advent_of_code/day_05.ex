1cbad8958ea2bc3845afd3bbbcbdc3fc2bc60880
freelon <freelon@gmx.net>
2021-12-05 09:38:02 +0100

    Reformatted.

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -9,22 +9,31 @@ defmodule AdventOfCode.Day05 do
     |> Enum.count(fn {_, v} -> v > 1 end)
   end
 
+  def part2(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line -> String.split(line, " -> ") end)
+    |> Enum.map(fn [start, finish] -> {point(start), point(finish)} end)
+    |> Enum.reduce(%{}, fn points, acc -> mark_points(points, acc, [:diag]) end)
+    |> Enum.count(fn {_, v} -> v > 1 end)
+  end
+
+  defp point(s) do
+    s
+    |> String.split(",")
+    |> Enum.map(fn l -> l |> String.to_integer() end)
+    |> case do
+      [a, b] -> {a, b}
+    end
+  end
+
   defp mark_points(line, acc, opts \\ []) do
     {{x1, y1}, {x2, y2}} = line
 
     cond do
-      x1 == x2 ->
-        y1..y2
-        |> Enum.reduce(acc, fn y, acc ->
-          Map.put(acc, {x1, y}, Map.get(acc, {x1, y}, 0) + 1)
-        end)
-
-      y1 == y2 ->
-        x1..x2
-        |> Enum.reduce(acc, fn x, acc -> Map.put(acc, {x, y1}, Map.get(acc, {x, y1}, 0) + 1) end)
-
-      :diag in opts ->
-        steps = abs(y1 - y2)
+      :diag in opts || x1 == x2 || y1 == y2 ->
+        steps = max(abs(y1 - y2), abs(x1 - x2))
         dx = round((x2 - x1) / steps)
         dy = round((y2 - y1) / steps)
 
@@ -40,24 +49,5 @@ defmodule AdventOfCode.Day05 do
       true ->
         acc
     end
-  end
-
-  defp point(s) do
-    s
-    |> String.split(",")
-    |> Enum.map(fn l -> l |> String.to_integer() end)
-    |> case do
-      [a, b] -> {a, b}
-    end
-  end
-
-  def part2(args) do
-    args
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.map(fn line -> String.split(line, " -> ") end)
-    |> Enum.map(fn [start, finish] -> {point(start), point(finish)} end)
-    |> Enum.reduce(%{}, fn points, acc -> mark_points(points, acc, [:diag]) end)
-    |> Enum.count(fn {_, v} -> v > 1 end)
   end
 end
