557555e30d0236b180db51a494bbebd85fa54b59
freelon <freelon@gmx.net>
2021-12-09 14:43:58 +0100

    Day 9 part 1.

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,5 +1,35 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(args) do
+    parse(args)
+    |> get_low_points()
+    |> Enum.map(fn {_, v} -> v + 1 end)
+    |> Enum.sum()
+  end
+
+  defp get_low_points(m) do
+    m
+    |> Enum.filter(fn {p, v} ->
+      neighbors(m, p) |> Enum.all?(fn other -> other > v end)
+    end)
+  end
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, y} ->
+      line
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.map(fn {v, x} -> {{x, y}, v |> String.to_integer()} end)
+    end)
+    |> Map.new()
+  end
+
+  defp neighbors(map, {x, y}) do
+    [{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}]
+    |> Enum.map(&Map.get(map, &1, 999))
   end
 
   def part2(_args) do
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -3,19 +3,21 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  defp input(), do: "2199943210
+3987894921
+9856789892
+8767896789
+9899965678"
 
-    assert result
+  test "part1" do
+    result = part1(input())
+
+    assert result == 15
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(input())
 
-    assert result
+    assert result == 1134
   end
 end
